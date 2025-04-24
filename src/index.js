1209469561ddc23a29c560b7feb52b5afe35dc76
divya prakash <2022ugce092@nitjsr.ac.in>
2025-04-24 10:47:12 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import connect from "./config/database.js";
 import Tweet from "./models/tweet.js";
 import hashTag from "./models/hashtags.js";
 import serverConfig from "./config/serverConfig.js";
+import TweetRepository from "./repository/tweet-repository.js";
 
 const app = express();
 
@@ -24,6 +25,11 @@ app.listen(serverConfig.PORT,async ()=>{
     //     text : "travel",
     //     tweets: ['680881f0269f1864c471dc4b']
     // })
-
     
+    const tweetRepo = new TweetRepository();
+    let tweets = await tweetRepo.getAll();
+    let tweet = await tweetRepo.deleteTweet({
+        '_id': '68088360ae21d3a3dae6e4e5'
+    });
+    console.log(tweet);
 })
